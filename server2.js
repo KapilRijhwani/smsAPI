92f12f9b3a1ca0a5ce892cd09154f6e881e22463
KapilRijhwani <kapil.rijhwani@gmail.com>
2018-10-29 21:25:15 +0530

    added pg v1

--- server2.js
+++ server2.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const app = express();
 const basicAuth = require('basic-auth');
+const pg = require('pg');
+
+const { Pool, Client } = require('pg')
+
+var config = {
+    user: 'postgres',
+    host: '127.0.0.1',
+    database: 'postgres',
+    password: 'password',
+    port: '5432'
+};
 
 
 app.use(express.json());
+
+// //Authorization of user using basicAuth module
+// //Sending HTTP 403 in case of failure
+// var auth = function(req, res, next){
+//     var user = basicAuth(req);
+//     if(!user || !user.name || !user.pass){
+//         res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+//         res.sendStatus(403);
+//         return;
+//     }
+//     if(user.name === "k" && user.pass === "r")    {
+//         next();
+       
+//     }
+    
+// }
 
 //Authorization of user using basicAuth module
 //Sending HTTP 403 in case of failure
@@ -14,12 +41,26 @@ var auth = function(req, res, next){
         res.sendStatus(403);
         return;
     }
-    if(user.name === "k" && user.pass === "r")    {
-        next();
-       
-    }
+    const pool = new Pool(config);
+    var result = null;
+
+    const queryAuthenticate = "SELECT CASE WHEN EXISTS(SELECT 1 FROM ACCOUNT WHERE USERNAME = '" 
+    + user.name +"' AND AUTH_ID = '" + user.pass + "') THEN 'TRUE' ELSE 'FALSE' END;";
     
+    pool.query(queryAuthenticate, (err, resAuthenticate) => {
+    result = resAuthenticate.rows[0].case;
+    pool.end();
+        if(result === 'TRUE'){
+            next();
+        } else {
+            res.set('WWW-Authenticate', 'Basic realm = Authorization Required');
+            res.sendStatus(403);
+            return ;
+        }
+    });        
 }
+
+
 
 function validateMissingParams(req){
     const from = req.body.from;
